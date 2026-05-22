f1fb34ca410f51ef6a042bc6ea5af20226bb2550
ShvetsIgor <ishveps@icloud.com>
2026-05-23 02:02:06 +0300

    auto: save CategoriesHub.tsx

--- src/features/categories/components/CategoriesHub.tsx
+++ src/features/categories/components/CategoriesHub.tsx
@@ -400,7 +400,7 @@ export function CategoriesHub() {
         >
           + Категорию
         </button>
-      </div>
+      </div>}
 
       {/* Category Editor Sheet */}
       <CategoryEditorSheet

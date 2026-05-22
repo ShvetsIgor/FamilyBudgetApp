909ff369cfcfe1dfcac83c34a50040893d155760
ShvetsIgor <ishveps@icloud.com>
2026-05-23 01:02:48 +0300

    auto: save librarySelectors.ts

--- src/features/categories/store/librarySelectors.ts
+++ src/features/categories/store/librarySelectors.ts
@@ -1,6 +1,6 @@
 import type { RootState } from '@/store/store';
 import type { CategoryType } from '@/shared/types';
-import { LIBRARY_FOLDERS } from '../config/categoryLabels';
+import { LIBRARY_FOLDERS } from '../config/libraryConfig';
 
 /**
  * Returns folder blueprints not yet activated by the user.

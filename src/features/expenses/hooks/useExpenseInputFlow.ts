60b711d576ceb4c8bccaad96af505b294b0dd1aa
ShvetsIgor <ishveps@icloud.com>
2026-05-23 10:33:19 +0300

    auto: save useExpenseInputFlow.ts

--- src/features/expenses/hooks/useExpenseInputFlow.ts
+++ src/features/expenses/hooks/useExpenseInputFlow.ts
@@ -30,7 +30,13 @@ import { useRouter } from 'next/navigation';
 import { useAppSelector, useAppDispatch } from '@/store/store';
 import { setSession, advanceStage, markSaved, clearSession } from '../store/inputSessionSlice';
 import type { InputStage, ExpenseInputSession } from '../store/inputSessionSlice';
-import { computeSuggestions, isHabitSuggestion, type ScoredSuggestion } from '../engine/suggestionEngine';
+import {
+  computeSuggestions,
+  isHabitSuggestion,
+  getConfidenceLevel,
+  type ScoredSuggestion,
+  type ConfidenceLevel,
+} from '../engine/suggestionEngine';
 import { parseQuickAdd } from '../utils/quickAddParser';
 import { inferStage } from './useInputSession';
 import { recordExpense } from '../store/suggestionMemorySlice';

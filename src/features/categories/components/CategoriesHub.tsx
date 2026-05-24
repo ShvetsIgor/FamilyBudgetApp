73b84d409eefa8d253a0433b35e8a75c7f264f4e
ShvetsIgor <ishveps@icloud.com>
2026-05-24 23:58:28 +0300

    auto: save CategoriesHub.tsx

--- src/features/categories/components/CategoriesHub.tsx
+++ src/features/categories/components/CategoriesHub.tsx
@@ -776,7 +776,7 @@ export function CategoriesHub() {
               cursor: 'text',
             }}
           >
-            {cat.name}
+            {t.cat(cat.name)}
           </span>
         )}
 

b19903dead8a59472a128b21f72e1c23ef4bd1d0
ShvetsIgor <ishveps@icloud.com>
2026-05-27 11:27:14 +0300

    auto: save store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -57,3 +57,4 @@ export type AppDispatch = typeof store.dispatch;
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppStore = () => useStore<RootState>();
